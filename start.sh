0b560c070aa07a8bf42684bd02cdce7fe9ba1f12
root <root@docker>
2018-03-22 10:36:58 +0300

    dgfg

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DISPLAY=:99
+/etc/init.d/xvfb start
+echo "Xvfb started at DISPLAY 99"
+/usr/bin/python extract.py >> cisco.log  
+/etc/init.d/xvfb stop
+echo "Finish checking"
+

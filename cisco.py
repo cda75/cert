26fa1ed28728b5392df803f7474ffeb6dcb16d9e
cda75 <dchestnov@mail.ru>
2018-07-09 10:55:09 +0300

    new xvfb permission

--- cisco.py
+++ cisco.py
@@ -13,7 +13,7 @@ from email.MIMEText import MIMEText
 
 
 cFile = 'config.cfg'
-admin = 'd.chestnov@inlinegroup.ru'
+admin = 'd.chestnov@inlinegroup.ru d.chestnov@gmail.com'
 reportFile = "cpapp_admin_cnt_xls_report_CertInd.xlsx"
 outputFile = 'report.xlsx'
 checkDays = 100
--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+SHELL=/bin/bash
+PATH=/home/dimka/bin:/home/dimka/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 export DISPLAY=:99
 /etc/init.d/xvfb start
 echo "Xvfb started at DISPLAY 99"
-/usr/bin/python cisco.py > log  
+date
+cd /home/dimka/cert
+/usr/bin/python /home/dimka/cert/cisco.py >> /home/dimka/cert/cisco.log 
 /etc/init.d/xvfb stop
+date
 echo "Finish checking"
 
